11bf35721ccbdf6ebfe33bd1c2a77e51c01fa231
AnyaPochi <podchinenova.as@gmail.com>
2024-03-13 11:10:27 +0500

    tasknotwork3

--- news/tasks.py
+++ news/tasks.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timedelta
+
 from celery import shared_task
+from project.celery import app
+from celery.schedules import crontab
 # import time
 # from .models import Category
 # @shared_task
@@ -9,10 +13,12 @@ from celery import shared_task
 from django.core.mail import EmailMultiAlternatives
 from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
-from news.models import PostCategory
+from news.models import PostCategory, Post, Category
 from django.template.loader import render_to_string
 from django.conf import settings
 
+
+# отправка писем подписчикам с новой новостью в разделе
 @shared_task
 def send_notifications(preview, pk, title, subscribers):
     html_content = render_to_string(
@@ -46,3 +52,36 @@ def notify_about_new_post(sender,instance,**kwargs):
             subscribers_emails += [s.email for s in subscribers]
 
         send_notifications.delay(instance.preview(), instance.pk, instance.title, subscribers_emails)
+
+# отправка еженедельных подборок
+@shared_task
+def my_job():
+    today=datetime.datetime.now()
+    last_week = today - timedelta(days=7)
+    posts = Post.objects.filter(time_in__gte=last_week)
+    categories = set(posts.values_list('category__name', flat=True))
+    subscribers = set(Category.objects.filter(name__in=categories).values_list('subscribers__email', flat=True))
+    html_content = render_to_string(
+        'daily_post.html',
+        {'link':settings.SITE_URL,
+                'posts': posts,
+                 }
+            )
+    msg = EmailMultiAlternatives(
+        subject='Статьи за неделю',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to = subscribers,
+    )
+
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+
+
+app.conf.beat_schedule = {
+    'weekly_news_subscribe': {
+        'task': 'my_job',
+        'schedule': crontab(hour=10, minute=52, day_of_week='wednesday'),
+    },
+}
+
