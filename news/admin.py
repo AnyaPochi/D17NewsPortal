1baa9bb6192075d9d30fbc73a0912040155ef6a4
AnyaPochi <podchinenova.as@gmail.com>
2024-03-22 21:48:10 +0500

    checkadminpanel

--- news/admin.py
+++ news/admin.py
@@ -2,12 +2,35 @@ from django.contrib import admin
 
 from .models import Category, Author, Post, PostCategory, Comment,UserCategory
 
+# создаём новый класс для представления постов в админке
+class PostAdmin(admin.ModelAdmin):
+
+    # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
+    list_display = ('title','time_in')  # оставляем только имя и цену товара
+    # list_display = [field.name for field in Post._meta.get_fields()]
+    list_filter = ('title','time_in','category')  # добавляем примитивные фильтры в нашу админку
+     # добавляем примитивные фильтры в нашу админку
+    # search_fields = ('title', 'category','time_in')  # тут всё очень похоже на фильтры из запросов в базу
+#не сработало
+# class PostInline(admin.TabularInline):
+#     model = Post.category.through
+
+
+# @admin.register(Category)
+# class CategoryAdmin(admin.ModelAdmin):
+#     model = Category
+#     inlines = [
+#         PostInline,
+#     ]
+
 admin.site.register(Category)
 admin.site.register(Author)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)
 admin.site.register(Comment)
 admin.site.register(UserCategory)
+
+
 
 
 
